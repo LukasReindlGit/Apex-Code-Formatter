e6f0d624cc501826cb04d29a961a14963f8ac6b8
Lukas Reindl <lukas.reindl@salesfive.com>
2022-02-15 16:08:04 +0100

    feat: added &= and && to operator list

--- apex-formatter.py
+++ apex-formatter.py
@@ -14,7 +14,7 @@ def regexSubstitution(line):
 
 
     # add spaces around operators
-    result = re.sub(r"([\*\/\+\-\=!<>|][\+\-\=!<>|]*)", r" \1 ", result)
+    result = re.sub(r"([\*\/\+\-\=\&!<>|][\+\-\=\&!<>|]*)", r" \1 ", result)
 
     # add spaces after comma, not before
     result = re.sub(r"\s*,\s*", r", ", result)
